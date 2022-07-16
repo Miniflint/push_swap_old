364a274c883e1b5d744a7045d5cde160c9104ecf
Tristan Goel <tgoel@c3r3s2.42lausanne.ch>
2022-07-16 12:13:03 +0200

    hehe

--- src/sorting/three_five.c
+++ src/sorting/three_five.c
@@ -6,7 +6,7 @@
 /*   By: tgoel <tgoel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 14:56:01 by tgoel             #+#    #+#             */
-/*   Updated: 2022/07/15 09:44:17 by tgoel            ###   ########.fr       */
+/*   Updated: 2022/07/16 12:12:35 by tgoel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,44 +16,59 @@
 
 void	three_numbers(t_all *all)
 {
-	int	nb[3];
-
-	nb[0] = all->stack_a->nb[0];
-	nb[1] = all->stack_a->nb[1];
-	nb[2] = all->stack_a->nb[2];
-	if (check_order(all))
-		return ;
-	if (nb[0] > nb[1] && nb[0] > nb[2])
-	{
+	if (all->stack_a->nb[0] > all->stack_a->nb[1]
+		&& all->stack_a->nb[0] > all->stack_a->nb[2])
 		ra(all, 1);
-		if (nb[1] > nb[2])
-			sa(all, 1);
-	}
-	else if (nb[0] < nb[1] && nb[0] < nb[2])
-	{
+	else if (all->stack_a->nb[1] > all->stack_a->nb[0]
+		&& all->stack_a->nb[1] > all->stack_a->nb[2])
+		rra(all, 1);
+	if (all->stack_a->nb[0] > all->stack_a->nb[1])
 		sa(all, 1);
-		ra(all, 1);
-	}
-	else
-	{
-		if (nb[0] > nb[1] && nb[1] < nb[2])
-			sa(all, 1);
-		else
-			rra(all, 1);
-	}
 }
 
-void	sort_five(t_all *all, int min)
+int	min_ind(t_all *all)
 {
-	if (all->stack_a->nb[3] == min)
-		rra(all, 1);
-	if (all->stack_a->nb[4])
-		if (all->stack_a->nb[4] == min)
-			rra(all, 1);
-	if (all->stack_a->nb[2] == min)
-		ra(all, 1);
-	if (all->stack_a->nb[1] == min)
-		ra(all, 1);
+	int	i;
+	int	min_size;
+	int	ret;
+
+	i = 0;
+	ret = 0;
+	min_size = all->stack_a->nb[0];
+	while (i < all->stack_a->current_size - 1)
+	{
+		if (all->stack_a->nb[++i] < min_size)
+		{
+			min_size = all->stack_a->nb[i];
+			ret = i;
+		}
+	}
+	return (ret);
+}
+
+void	two_push(t_all *all)
+{
+	int	i;
+	int	min_i;
+
+	while (all->stack_a->current_size - 3)
+	{
+		i = 0;
+		min_i = min_ind(all);
+		if (min_i == -1)
+			handle_error("error damn", all);
+		if (min_i == 1)
+			sa(all, 1);
+		else if (min_i > all->stack_a->current_size / 2)
+		{
+			while (i++ < all->stack_a->current_size - min_i)
+				rra(all, 1);
+		}
+		else
+			while (i++ < min_i)
+				ra(all, 1);
+		pb(all);
+	}
 }
 
 void	sort_to_five(t_all *all, int nb)
@@ -63,8 +78,7 @@ void	sort_to_five(t_all *all, int nb)
 	while (nb > 3)
 	{
 		min = get_min(all->stack_a);
-		sort_five(all, min);
-		pb(all);
+		two_push(all);
 		nb--;
 	}
 	three_numbers(all);
